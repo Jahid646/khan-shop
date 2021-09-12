2817b4ff51632feb7c3aa002ce57a6b49c4cc81a
Jahid Hasan <jhasan646@gmail.com>
2021-09-13 04:13:23 +0600

    Search area customize

--- js/app.js
+++ js/app.js
@@ -23,8 +23,8 @@ const showProducts = (products) => {
       <h5><b>Ratings: ${product.rating.rate}<b></h5>
       <h5><b>Ratings Count: ${product.rating.count}.<b></h5>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-dark">add to cart</button>
+      <button id="details-btn" class="btn btn-red">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
